82182583d18855100463883fdb69b97feb9fc663
Pedro Cunha <cunhaac@c2.lan>
2024-12-01 23:35:31 +0000

    first commit

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -3,100 +3,40 @@ import themes from '../../themes.json';
 import { history } from '../stores/history';
 import { theme } from '../stores/theme';
 
-const hostname = window.location.hostname;
+const hostname = 'c2';
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
   help: () => 'Available commands: ' + Object.keys(commands).join(', '),
   hostname: () => hostname,
-  whoami: () => 'guest',
+  whoami: () => 'Pedro Cunha',
   date: () => new Date().toLocaleString(),
-  vi: () => `why use vi? try 'emacs'`,
-  vim: () => `why use vim? try 'emacs'`,
-  emacs: () => `why use emacs? try 'vim'`,
+  vi: () => `why use vi? try 'nano'`,
+  vim: () => `why use vim? try 'nano'`,
+  nano: () => `why use emacs? try 'vim'`,
+  pwd: () => `/var/www/html`,
   echo: (args: string[]) => args.join(' '),
   sudo: (args: string[]) => {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-
     return `Permission denied: unable to run the command '${args[0]}' as root.`;
-  },
-  theme: (args: string[]) => {
-    const usage = `Usage: theme [args].
-    [args]:
-      ls: list all available themes
-      set: set theme to [theme]
-
-    [Examples]:
-      theme ls
-      theme set gruvboxdark
-    `;
-    if (args.length === 0) {
-      return usage;
-    }
-
-    switch (args[0]) {
-      case 'ls': {
-        let result = themes.map((t) => t.name.toLowerCase()).join(', ');
-        result += `You can preview all these themes here: ${packageJson.repository.url}/tree/master/docs/themes`;
-
-        return result;
-      }
-
-      case 'set': {
-        if (args.length !== 2) {
-          return usage;
-        }
-
-        const selectedTheme = args[1];
-        const t = themes.find((t) => t.name.toLowerCase() === selectedTheme);
-
-        if (!t) {
-          return `Theme '${selectedTheme}' not found. Try 'theme ls' to see all available themes.`;
-        }
-
-        theme.set(t);
-
-        return `Theme set to ${selectedTheme}`;
-      }
-
-      default: {
-        return usage;
-      }
-    }
-  },
-  repo: () => {
-    window.open(packageJson.repository.url, '_blank');
-
-    return 'Opening repository...';
   },
   clear: () => {
     history.set([]);
-
     return '';
   },
   email: () => {
     window.open(`mailto:${packageJson.author.email}`);
-
     return `Opening mailto:${packageJson.author.email}...`;
-  },
-  donate: () => {
-    window.open(packageJson.funding.url, '_blank');
-
-    return 'Opening donation url...';
   },
   weather: async (args: string[]) => {
     const city = args.join('+');
-
     if (!city) {
       return 'Usage: weather [city]. Example: weather Brussels';
     }
 
     const weather = await fetch(`https://wttr.in/${city}?ATm`);
-
     return weather.text();
   },
-  exit: () => {
-    return 'Please close the tab to exit.';
-  },
+  exit: () => 'Please close the tab to exit.',
   curl: async (args: string[]) => {
     if (args.length === 0) {
       return 'curl: no URL provided';
@@ -107,20 +47,46 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
     try {
       const response = await fetch(url);
       const data = await response.text();
-
       return data;
     } catch (error) {
       return `curl: could not fetch URL ${url}. Details: ${error}`;
     }
   },
-  banner: () => `
-███╗   ███╗██╗  ██╗████████╗████████╗███████╗██████╗
-████╗ ████║██║  ██║╚══██╔══╝╚══██╔══╝╚════██║╚════██╗
-██╔████╔██║███████║   ██║      ██║       ██╔╝ █████╔╝
-██║╚██╔╝██║╚════██║   ██║      ██║      ██╔╝ ██╔═══╝
-██║ ╚═╝ ██║     ██║   ██║      ██║      ██║  ███████╗
-╚═╝     ╚═╝     ╚═╝   ╚═╝      ╚═╝      ╚═╝  ╚══════╝ v${packageJson.version}
-
+  banner: () => `                                                                   
+           __.                                                                                                  
+        .-".'                      .--.            _..._                                            
+      .' .'                     .'    \\       .-""  __ ""-.                                          
+     /  /                     .'       : --..:__.-""  ""-. \\                                         
+    :  :                     /         .d$$    sbp_.-""-:_:                                                 
+    :   \\                    \\  T--...-: : d$b  :d$b                                                    
+     \\   \`.                   \\  \`..'    $ $  $ $                                                 
+      \`.   "-.                 ).        : T$P  :T$P                                                           
+        \\..---^..             /           \`-'    \`._\`._                                                         
+       .'        "-.       .-"                     T$$$b                                              
+      /             "-._.-"               ._        '^'                                                       
+     :                                    \\.\`         /                                   
+     :                                -.   \\\`."-._.-'-'                                                   
+    :                                 .'\   \\ \\ \\ \\                                   
+    :  :                             /:  \\   \\ \\ .                                       
+   :   :                            ,  ;  \`.  \`.;  :                                      
+   ;    \\        ;                     ;    "-._:  ;                                             
+  :      \`.      :                     :         \\/                                                   
+  ;       /"-.    ;                    :                                        
+ :       /    "-. :                  : ;                              
+ :     .'        T-;                 ; ;        
+ ;    :          ; ;                /  :          
+ ;    ;          : :              .'    ;       
+:    :            ;:         _..-"\\     :       
+:     \\           : ;       /      \\     ;      
+;    . '.         '-;      /        ;    :      
+;  \\  ; :           :     :         :    '-.        
+'.._L.:-'           :     ;          ;    . \`.             
+                     ;    :          :  \\  ; :   
+                     :    '-..       '.._L.:-'      
+                      ;     , \`.                
+                      :   \\  ; :                     
+                      '..__L.:-'  
+  
 Type 'help' to see list of available commands.
-`,
+  `
 };
